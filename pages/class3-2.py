900869290dab1fbe4a4c21d93c1b49b4b8c32031
Lin-77777 <yclrx78@gmail.com>
2025-02-05 16:58:55 +0800

    day3

--- pages/class3-2.py
+++ pages/class3-2.py
@@ -6,3 +6,10 @@ st.write("數字金字塔")
 n = n + 1
 for i in range(n):
     st.write(str(i) * i)
+
+
+st.write("---")
+st.title("文字輸入元件")
+# st.text_input()指令格式 st.text_input(輸入欄位標題,value=""預設顯示文字"")
+a = st.text_input("請輸入文字", value="這是一個預設顯示文字")
+st.write(f"你輸入的文字是:{a}")
