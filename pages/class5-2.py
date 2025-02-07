9c98f8172f9dc98e2e516de6059979b54962db66
Lin-77777 <yclrx78@gmail.com>
2025-02-07 14:21:32 +0800

    a

--- pages/class5-2.py
+++ pages/class5-2.py
@@ -1,6 +1,14 @@
 import streamlit as st
+from openai import OpenAI
+import dotenv  # pip install python-dotenv -U
+import os
 
-st.title("猜謎遊戲")
+dotenv.load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+
+st.title("聊天AI")
 if "history" not in st.session_state:
     st.session_state.history = []
 
@@ -13,7 +21,17 @@ message = st.chat_input("請輸入文字")  # 聊天輸入框元件
 
 if message:
     st.session_state.history.append({"role": "user", "content": message})
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "developer",
+                "content": "你是一個小幫手，只能用繁體中文(#zh-TW)和日文(#ja-JP)回應後續對話",
+            }
+        ]
+        + st.session_state.history,
+    )
     st.session_state.history.append(
-        {"role": "assistant", "content": f"你輸入的內容是:{message}"}
+        {"role": "assistant", "content": completion.choices[0].message.content}
     )
     st.rerun()  # 重新整理頁面
